a7f9be3788d574f35960bbc31fdfc997bca0265b
wndud5814 <wndud5814@naver.com>
2023-11-30 09:40:10 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      <img
+        className="w-96 h-96 animate__animated animate__zoomInDown animate__infinite	infinite"
+        src="./images/hms.png"
+        alt="hms"
+      />
+    </div>
+  );
 };
 
 export default App;
